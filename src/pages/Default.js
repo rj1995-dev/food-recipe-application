355643aee41c9ae0460a1df297488f0aec27a10c
rj1995-dev <ram95.jana@gmail.com>
2019-08-17 01:13:49 +0530

    finish commit

---
+++ src/pages/Default.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
+export default class Default extends Component {
+  render() {
+    return (
+      <Header title="404" styleClass="default-hero">
+        <h2 className="text-light text-uppercase">you are in wrong place.</h2>
+        <Link to="/" className="text-uppercase btn btn-primary btn-lg mt-3">
+          return home
+        </Link>
+      </Header>
+    );
+  }
+}
